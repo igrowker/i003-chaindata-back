cfc8eab940546c9dff94a236e0cc66b8a9575eae
ElvisCalero13 <elviscaleromanueles@gmail.com>
2024-09-17 03:39:07 -0500

    add libraries for data process

--- routers/user.py
+++ routers/user.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from typing import List
 from pydantic import EmailStr
 from database.db_dependency import session
-from services import user
-from schemas.user import UserResponse, UserRequest
+from services import user as service
+from schemas import user as schema
 from const import PATH_USERS, TAG_USERS
 
 userRouter = APIRouter(
@@ -12,63 +12,63 @@ userRouter = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@userRouter.post("/add/", response_model=UserResponse, status_code=201)
-def create_user(request: UserRequest, db:user.Session = Depends(session)) -> UserResponse:
+@userRouter.post("/add/", response_model=schema.UserResponse, status_code=201)
+def create_user(request: schema.UserCreate, db:service.Session = Depends(session)) -> schema.UserResponse:
     """
     # Create new user
     """
-    validation = user.select_user_by_email(db, request.email)
-    if validation is not None:
-        raise HTTPException(status_code=404, detail=f"User with email:{request.email} already exists")
-    result = user.insert_user(db, request)
-    return result
-
-@userRouter.get("/", response_model=List[UserResponse])
-def get_all_users(db:user.Session = Depends(session)) -> List[UserResponse]:
+    try:
+        _user = service.insert_user(db, request)
+        return _user
+    except Exception as e:
+        raise HTTPException(status_code=409, detail="the create gone wrong")
+    
+@userRouter.get("/", response_model=List[schema.UserResponse])
+def get_all_users(db:service.Session = Depends(session)) -> List[schema.UserResponse]:
     """
     # Get a list of all users 
     """
-    result = user.select_users(db)
+    result = service.select_users(db)
     return result
     
-@userRouter.get("/id/{user_id}", response_model=UserResponse)  
-def get_user_by_id(user_id:int, db:user.Session = Depends(session)) -> UserResponse:
+@userRouter.get("/id/{user_id}", response_model=schema.UserResponse)  
+def get_user_by_id(user_id:int, db:service.Session = Depends(session)) -> schema.UserResponse:
     """
     # Get a user by id
     """
-    result = user.select_user_by_id(db, user_id)
+    result = service.select_user_by_id(db, user_id)
     if not result:
         raise HTTPException(status_code=404, detail=f"User with id:{user_id} not found")
     return result
 
-@userRouter.get("/email/{email}", response_model=UserResponse)  
-def get_user_by_email(email:EmailStr, db:user.Session = Depends(session)) -> UserResponse:
+@userRouter.get("/email/{email}", response_model=schema.UserResponse)  
+def get_user_by_email(email:EmailStr, db:service.Session = Depends(session)) -> schema.UserResponse:
     """
     # Get a user by email
     """
-    result = user.select_user_by_email(db, email)
+    result = service.select_user_by_email(db, email)
     if not result:
         raise HTTPException(status_code=404, detail=f"User with email:{email} not found")
     return result
 
-@userRouter.patch("/update/{user_id}")
-async def update_book(user_id:int, request: UserRequest, db:user.Session = Depends(session)):
+@userRouter.patch("/update/{user_id}", response_model=schema.UserResponse)
+async def update_book(user_id:int, request: schema.UserUpdate, db:service.Session = Depends(session)) -> schema.UserResponse:
     """
     # Update a user by id
     """
     try:
-        _user = user.update_user(db, user_id, request)
+        _user = service.update_user(db, user_id, request)
         return _user
     except Exception as e:
-        return HTTPException(status_code=304, detail="the updated gone wrong")
+        raise HTTPException(status_code=404, detail="the updated gone wrong")
 
-@userRouter.delete("/delete/{user_id}")
-async def delete_book(user_id:int, db:user.Session = Depends(session)):
+@userRouter.delete("/delete/{user_id}", response_model=schema.UserResponse)
+async def delete_book(user_id:int, db:service.Session = Depends(session)) -> schema.UserResponse:
     """
     # Delete a user by id
     """
     try:
-        _user = user.delete_book(db, user_id)
+        _user = service.delete_book(db, user_id)
         return _user
     except Exception as e:
-        return HTTPException(status_code=304, detail="the deleted gone wrong")
+        raise HTTPException(status_code=404, detail="the deleted gone wrong")
--- schemas/user.py
+++ schemas/user.py
@@ -3,6 +3,15 @@ from pydantic import BaseModel, EmailStr
 
 class UserResponse(BaseModel):
     user_id: int | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    email: EmailStr | None = None
+    password: str | None = None
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
     first_name: str
     last_name: str
     email: EmailStr
@@ -10,12 +19,11 @@ class UserResponse(BaseModel):
 
     class Config:
         orm_mode = True
-        
-class UserRequest(BaseModel):
-    first_name: str
-    last_name: str
-    email: EmailStr
-    password: str
+
+class UserUpdate(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    password: str | None = None
 
     class Config:
         orm_mode = True
